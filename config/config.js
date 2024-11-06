fd343862b885cfe1c12a5ddb816fe045631394bf
djksanrs <chinh99562@donga.edu.vn>
2024-11-06 08:57:47 +0700

    Add dotenv for security reason

---
+++ config/config.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const uri = process.env.MONGODB_API;
+
+export function connectMongoDB() {
+  mongoose.connect(uri)
+    .then(() => console.log("Connected to mongose database"))
+    .catch(error => console.log("Cannot connect to the database", error));
+}
+
+
