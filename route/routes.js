cd80147eb91ef9d447f22526caba4ad33df76c71
djksanrs <chinh99562@donga.edu.vn>
2024-11-06 08:58:46 +0700

    Separate these logics to controller

--- route/routes.js
+++ route/routes.js
@@ -4,34 +4,39 @@
 //POST /api/auth/login
 //```
 import express from 'express';
-import { User } from '../model/models.js';
+import { login, register, findUserById } from '../controller/userController.js';
+import {getAllRecipes, getRecipeById, saveRecipe} from '../controller/recipeController.js';
 
-const router = express.Router();
+export const router = express.Router();
 
-router.post('/api/auth/login', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (user && user.password == password) {
-      res.status(200).json({ message: "Login successful", user });
-    } else {
-      res.status(401).json({ message: "Invalid username or password" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Error while logging", error });
-  }
-});
+/*      ------ Authentication and user query ------       */
+router.post('/api/auth/login', login);
+router.post('/api/auth/register', register);
+router.post('/api/users/:id', findUserById);
 
-router.post('/api/auth/register', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const newUser = new User({
-      username,
-      password
-    });
-    await newUser.save();
-    res.status(201).json({ message: 'User registered successfully', user: newUser });
-  } catch (error) {
-    res.status(500).json({ message: "Error while registering new user", error });
-  };
-});
+
+//POST /api/users/follow/:id
+//DELETE /api/users/unfollow/:id
+//GET /api/users/:id/followers
+//GET /api/users/:id/following
+//```
+//
+//### Recipe Operations
+//```javascript
+//POST /api/recipes
+//GET /api/recipes
+
+
+/*      ------ RECIPE ------       */
+router.post("/api/recipes/", saveRecipe);
+router.get("/api/recipes", getAllRecipes);
+router.get("/api/recipes/:id", getRecipeById);
+
+//PUT /api/recipes/:id
+//DELETE /api/recipes/:id
+//GET /api/recipes/search
+//GET /api/recipes/categories
+//POST /api/recipes/:id/like
+//POST /api/recipes/:id/rate
+//POST /api/recipes/:id/comment
+//```
