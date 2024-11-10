3d8aa5005075214187eb8b4da4efe95cd94b637b
djksanrs <chinh99562@donga.edu.vn>
2024-11-10 16:15:51 +0700

    Group methods into single class

--- controller/recipeController.js
+++ controller/recipeController.js
@@ -1,41 +1,48 @@
-import {Recipe} from '../model/models.js';
+import { Recipe } from "../model/models.js";
 
-
-export const saveRecipe = async (req, res) => {
-  const { userId, title, description, ingredients, category } = req.body;
-  const newRecipe = new Recipe({
-    userId: userId,
-    title: title, description: description, ingredients: ingredients, category: category,
-  });
-  try {
-    newRecipe.save();
-    const savedRecipe = await newRecipe.save();
-    res.status(201).json(savedRecipe);
-  } catch (error) {
-    res.status(500).json({ message: "Error while trying to post a new recipe", error });
-  }
-};
-
-export const getAllRecipes = async (_req, res) => {
-  try {
-    const recipes = await Recipe.find();
-    res.status(201).json(recipes);
-  } catch (error) {
-    res.status(500).json({message: error});
-  }
-};
-
-export const getRecipeById = async (req, res) => {
-  const id = req.params.id;
-  try {
-    const recipe = await Recipe.findById(id);
-    if (recipe) {
-      res.status(200).json(recipe);
-    } else {
-      res.status(404).json({ message: "Recipe not found" });
+class RecipeController {
+  static async saveRecipe(req, res) {
+    const { userId, title, description, ingredients, category } = req.body;
+    const newRecipe = new Recipe({
+      userId: userId,
+      title: title,
+      description: description,
+      ingredients: ingredients,
+      category: category,
+    });
+    try {
+      newRecipe.save();
+      const savedRecipe = await newRecipe.save();
+      res.status(201).json(savedRecipe);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error while trying to post a new recipe", error });
     }
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server", error });
   }
-};
 
+  static async getAllRecipes(req, res) {
+    try {
+      const recipes = await Recipe.find();
+      res.status(201).json(recipes);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+
+  static async getRecipeById(req, res) {
+    const id = req.params.id;
+    try {
+      const recipe = await Recipe.findById(id);
+      if (recipe) {
+        res.status(200).json(recipe);
+      } else {
+        res.status(404).json({ message: "Recipe not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server", error });
+    }
+  }
+}
+
+export default RecipeController;
