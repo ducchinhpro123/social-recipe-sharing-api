bef9f1db4c478de0cb1f19c3fb2ae7090ce8f6e7
djksanrs <chinh99562@donga.edu.vn>
2024-11-10 21:22:08 +0700

    Define dirname to fetch images

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import express    from 'express';
-import bodyParser from 'body-parser';
+import express           from 'express';
+import bodyParser        from 'body-parser';
+import path              from 'path';
+import { fileURLToPath } from 'url';
 
 import { router }         from './route/routes.js';
 import { connectMongoDB } from './config/config.js';
@@ -9,10 +11,13 @@ const app = express();
 
 connectMongoDB();
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 app.use('/', router);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
+app.use('/public/images/', express.static(path.join(__dirname, 'public', 'images')));
 app.use(express.static('./public'));
 
 app.listen(PORT, () => {
