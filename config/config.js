11188d859988cb5f500c7eb212dc6323e1280aac
djksanrs <chinh99562@donga.edu.vn>
2024-11-16 21:41:14 +0700

    Correct grammar

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const uri = process.env.MONGODB_API;
 
 export function connectMongoDB() {
   mongoose.connect(uri)
-    .then(() => console.log("Connected to mongose database"))
+    .then(() => console.log("Connected to mongoose database"))
     .catch(error => console.log("Cannot connect to the database", error));
 }
 
