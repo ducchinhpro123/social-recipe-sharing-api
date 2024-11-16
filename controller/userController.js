e677a30aedfe9229420f84468fd31883fad8d145
djksanrs <chinh99562@donga.edu.vn>
2024-11-16 21:41:58 +0700

    Implement a method to add a specific recipe id to user's favorites list

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,36 @@
 import { User } from "../model/models.js";
+import { Recipe } from "../model/models.js";
 
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 
 class UserController {
+
+  static async saveFavorites(req, res) {
+    // favorites: [{ type: Schema.Types.ObjectId, ref: 'Recipe'}],
+    const { username, recipeId } = req.body;
+
+    try {
+      const recipe = await Recipe.findById(recipeId);
+      const user = await User.findOne({username});
+
+      if (recipe && user) {
+        const isFavorite = user.favorites.includes(recipeId);
+        if (isFavorite) {
+          user.favorites.pull(recipeId);
+        } else {
+          user.favorites.push(recipeId);
+        }
+        await user.save();
+        return res.status(200).json({message: "You added a new recipe to your favorites recipe list."});
+      } else {
+        return res.status(404).json({message: "You missed recipe id or username"});
+      }
+    } catch (error) {
+      res.status(500).json({message: "There something be wrong ", error});
+    }
+  }
 
   static async getAllUsers(req, res) {
     const users = await User.find();
@@ -24,7 +50,7 @@ class UserController {
       const isValidPass = await bcrypt.compare(password, user.password);
 
       if (user && isValidPass) {
-        const token = jwt.sign({id: user._id}, "159d86da542f791a23cca61d76ae243c0464fbb594212e2332fb5946b23a18fd", 
+        const token = jwt.sign({id: user._id}, "159d86da542f791a23cca61d76ae243c0464fbb594212e2332fb5946b23a18fd",
           {expiresIn: "1h"});
 
         return res.status(200).json({ message: "Login successful", token, user });
@@ -55,6 +81,7 @@ class UserController {
         .json({ message: "Error while registering new user", error });
     }
   }
+
   static async findUserById(req, res) {
     const id = req.params.id;
     try {
