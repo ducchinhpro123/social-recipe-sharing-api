52ff19b405915a1b2dfa8bfac8ecc3b798e88865
djksanrs <chinh99562@donga.edu.vn>
2024-11-16 16:27:13 +0700

    Encrypted password and provide the json token if everything is fine

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,11 @@
 import { User } from "../model/models.js";
 
-import {inspect} from "util";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 class UserController {
+
   static async getAllUsers(req, res) {
     const users = await User.find();
     res.status(200).json(users);
@@ -11,11 +14,26 @@ class UserController {
   static async login(req, res) {
     try {
       const { username, password } = req.body;
+
       const user = await User.findOne({ username });
-      if (user && user.password === password) {
-        res.status(200).json({ message: "Login successful", user });
+
+      // if (user.password !== password) {
+      //   return res.status(400).json({message: "Invalid username or password"});
+      // }
+      if (!user) {
+        return res.status(401).json({message: "User not found."});
+      }
+
+      const isValidPass = await bcrypt.compare(password, user.password);
+      console.log(`is valid password? ${isValidPass}`)
+
+      if (user && isValidPass) {
+        const token = jwt.sign({id: user._id}, "159d86da542f791a23cca61d76ae244c0464fbb594212e2332fb5946b23a18fd", 
+          {expiresIn: "1h"});
+
+        return res.status(200).json({ message: "Login successful", token });
       } else {
-        res.status(401).json({ message: "Invalid username or password" });
+        return res.status(401).json({ message: "Invalid username or password" });
       }
     } catch (error) {
       res.status(500).json({ message: "Error while logging", error });
@@ -25,14 +43,16 @@ class UserController {
   static async register(req, res) {
     try {
       const { username, password } = req.body;
-      const newUser = new User({
-        username,
-        password,
-      });
+      const userExists = await User.findOne({username});
+      if (userExists) {
+        res.status(409).json({message: "User with username " + username + " already exists."});
+      }
+      const encryptedPass = await bcrypt.hash(password, 10);
+
+      const newUser = new User({ username, password: encryptedPass });
       await newUser.save();
-      res
-        .status(201)
-        .json({ message: "User registered successfully", user: newUser });
+
+      res.status(201).json({ message: "User registered successfully", user: newUser });
     } catch (error) {
       res
         .status(500)
