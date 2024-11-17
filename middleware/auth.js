c90a9324064aa2b0951cb41a34a2fc21b72954c8
djksanrs <chinh99562@donga.edu.vn>
2024-11-17 19:05:38 +0700

    use express-session to save user account

---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+export function isAuthenticated(req, res, next) {
+        if (req.session.user) {
+                return next();
+        }
+        else {
+                res.redirect('/admin/login');
+        }
+
+}
