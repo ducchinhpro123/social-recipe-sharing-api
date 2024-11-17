d58e6f0e7a0379dbba1d2304e6ec151a10da5341
djksanrs <chinh99562@donga.edu.vn>
2024-11-17 21:39:56 +0700

    New route and reformat the file

--- route/route_admin.js
+++ route/route_admin.js
@@ -4,11 +4,13 @@ import {isAuthenticated} from "../middleware/auth.js";
 
 export const adminRouter = express.Router();
 
-adminRouter.get("/login",               AdminController.login);
-adminRouter.post("/login-request",      AdminController.loginRequest);
-adminRouter.get("/home",                isAuthenticated, AdminController.home);
-adminRouter.get("/recipe/update/:id",   isAuthenticated, AdminController.updatePage);
-adminRouter.post("/recipe/update/",     isAuthenticated, AdminController.updateRecipePost);
-adminRouter.get("/recipe/delete/:id",   isAuthenticated, AdminController.deleteRecipe);
+adminRouter.get("/login",                       AdminController.login);
+adminRouter.post("/login-request",              AdminController.loginRequest);
+adminRouter.get("/home",                        isAuthenticated, AdminController.home);
+adminRouter.get("/recipe/update/:id",           isAuthenticated, AdminController.updatePage);
+adminRouter.post("/recipe/update/",             isAuthenticated, AdminController.updateRecipePost);
+adminRouter.get("/recipe/delete/:id",           isAuthenticated, AdminController.deleteRecipe);
+adminRouter.get("/recipe/add-new-recipe/",      isAuthenticated, AdminController.addNewRecipe);
+adminRouter.post("/recipe/add-new-recipe/",     isAuthenticated, AdminController.addNewRecipeRequest);
 
 
